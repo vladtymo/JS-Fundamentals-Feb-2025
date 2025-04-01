b2c6c40808b0831c96ee16f2bd44d0e7dd61b487
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-04-01 18:09:04 +0300

    HTML templator example

--- 17 Shop App/main.js
+++ 17 Shop App/main.js
@@ -4,23 +4,19 @@ const dummyApi = 'https://dummyjson.com/products';
 const grid = document.querySelector('.grid');
 const loadBtn = document.getElementById('load-more-btn');
 
+const cardSource = document.getElementById('cardTemplate').innerHTML;
+const cardtemplate = Handlebars.compile(cardSource);
+
 let page = 0;
 let pageSize = 5;
 
 function loadProducts() {
+
     fetch(dummyApi + `?limit=${pageSize}&skip=${page * pageSize}`)
         .then(res => res.json())
         .then(data => {
             for (const i of data.products) {
-                grid.innerHTML += ` <div class="col">
-                                    <div class="card h-100">
-                                        <img src="${i.thumbnail}" class="card-img-top" alt="${i.title}">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${i.title}</h5>
-                                            <p class="card-text">${i.price}$ - ${i.category}</p>
-                                        </div>
-                                    </div>
-                                </div>`
+                grid.innerHTML += cardtemplate(i); // use template instead of 'HTML'
             }
             page++;
         });
