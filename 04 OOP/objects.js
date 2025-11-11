9c9323d902a2e4b6ac37578b7db1fce3f04d9d71
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-11-11 10:39:04 +0200

    Objects example plus

--- 04 OOP/objects.js
+++ 04 OOP/objects.js
@@ -20,13 +20,15 @@ order.discount = 15;
 
 // 2 - using {}
 var order = {
-    // initi with properties
-    number: 10,
-    discount: 15,
-    client: "John Smith",
-    complete: false,
-    price: 1200,
+  // initi with properties
+  number: 10,
+  discount: 15,
+  client: "John Smith",
+  complete: false,
+  price: 1200,
 };
+
+order.totalPrice *= 0.9; // apply 10% discount
 
 console.log("Order status:", order.complete ? "Done" : "In Progress");
 
@@ -36,26 +38,26 @@ console.log("Order type:", typeof order); // object
 order.date = "20/1/2023";
 order.products = ["Apple", "Cherry", "Banana", "Pineapple"];
 order.address = {
-    city: "Rivne",
-    street: "Soborna",
-    builing: "5a"
+  city: "Rivne",
+  street: "Soborna",
+  builing: "5a",
 };
 
 console.log("Shipping street:", order.address.street);
 
 let car = {
-    model: "Audi A6",
-    year: 2007,
-    color: "Red",
-    engine: {
-        power: 240,
-        volume: 3.0,
-        type: "TSI"
-    },
-    sto: ['10.10.2019', '10.10.2020', '04.10.2022']
-}
+  model: "Audi A6",
+  year: 2007,
+  color: "Red",
+  engine: {
+    power: 240,
+    volume: 3.0,
+    type: "TSI",
+  },
+  sto: ["10.10.2019", "10.10.2020", "04.10.2022"],
+};
 
-car.sto.push('26.12.2023');
+car.sto.push("26.12.2023");
 console.log(car.engine.type);
 
 // -- override values
@@ -66,8 +68,12 @@ order.price += 30;
 // 2 - object["property"]
 
 console.log(`Order info: ${order["number"]} at ${order["date"]}`);
-console.log(`[${order.number}] ${order.client} ordered ${order.products.length} items!`);
-console.log(`Client address: ${order.address.city}, ${order.address.street}...`);
+console.log(
+  `[${order.number}] ${order.client} ordered ${order.products.length} items!`
+);
+console.log(
+  `Client address: ${order.address.city}, ${order.address.street}...`
+);
 
 order.speed = 100;
 // -- delete existin prop
@@ -78,14 +84,14 @@ console.log("Value:", order.speed); // undefined
 
 // -- using object as a parameters
 function showOrder(order) {
-    console.log("Order:", order.number);
-    console.log("Products:", order.products);
+  console.log("Order:", order.number);
+  console.log("Products:", order.products);
 
-    //console.log("this in function scope:", this); // window, undefined in strict mode
+  //console.log("this in function scope:", this); // window, undefined in strict mode
 }
 
 function addNewItem(order, item) {
-    order.products.push(item);
+  order.products.push(item);
 }
 
 addNewItem(order, "Lemon");
@@ -95,9 +101,9 @@ showOrder(order);
 const coord = { x: 4, y: 9, z: 12 };
 
 const coords = [
-    { x: 1, y: 4 },
-    { x: 9, y: 7 },
-    { x: 0, y: 8 }
+  { x: 1, y: 4 },
+  { x: 9, y: 7 },
+  { x: 0, y: 8 },
 ];
 
 console.log("X2:", coords[2].y); // 8
@@ -106,7 +112,7 @@ console.log("X2:", coords[2].y); // 8
 console.warn("Order properties:");
 
 for (const key in order) {
-    console.log(key + ": ... " + order[key]);
+  console.log(key + ": ... " + order[key]);
 }
 
 console.log("Has speed property:", order.hasOwnProperty("number"));
@@ -115,23 +121,23 @@ console.log("Keys:", Object.values(order));
 
 // ------------ methods = function inside object
 const product = {
-    // proeprties
-    name: 'Radeon RX 6600',
-    price: 11499,
-    rating: 4.8,
-    inStock: true,
+  // proeprties
+  name: "Radeon RX 6600",
+  price: 11499,
+  rating: 4.8,
+  inStock: true,
 
-    // object methods
-    applyDiscount: function (value) {
-        // [this] - reference to this object
-        this.price -= this.price / 100 * value;
+  // object methods
+  applyDiscount: function (value) {
+    // [this] - reference to this object
+    this.price -= (this.price / 100) * value;
 
-        // [this] = [product]
-        console.log("this in object scope:", this);
-    },
-    show() {
-        console.log(`Name: ${this.name} Price: ${this.price}$`);
-    }
+    // [this] = [product]
+    console.log("this in object scope:", this);
+  },
+  show() {
+    console.log(`Name: ${this.name} Price: ${this.price}$`);
+  },
 };
 
 // [this] - context-related keyword
@@ -145,3 +151,18 @@ console.log("this in global scope:", this); // window object
 product.show();
 product.applyDiscount(15); // -15%
 product.show();
+
+// --- Example
+
+let product1 = {
+  name: "Iphone 14",
+  price: 999,
+};
+let product2 = {
+  name: "Samsung S23",
+  price: 899,
+};
+
+let cart = [product1, product2];
+
+console.log("Price: ", cart[1].price, "$");
